25d291737ad7e9ed491e90c3d32df4cffcde9048
Karan <kstkaranthakur03@gmail.com>
2024-07-01 03:28:30 +0530

    completing user routes and admin routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getMyProfile, login } from "../controllers/user";
+import { getMyProfile, login } from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated, logout } from "../middlewares/auth.js";
-import { loginValidator, registerValidator, validateHandler } from "../lib/validators.js";
+import { acceptRequestValidator, loginValidator, registerValidator, sendRequestValidator, validateHandler } from "../lib/validators.js";
 
 const app = express.Router();
 
@@ -19,5 +19,24 @@ app.get("/search", searchUser);
 
 //after this only authenticated user must access further
 app.use(errorMiddleware);//at the end
+
+app.put(
+    "/sendrequest",
+    sendRequestValidator(),
+    validateHandler,
+    sendFriendRequest
+);
+
+app.put(
+    "/acceptrequest",
+    acceptRequestValidator(),
+    validateHandler,
+    acceptFriendRequest
+  );
+  
+  app.get("/notifications", getMyNotifications);
+  
+  app.get("/friends", getMyFriends);
+  
 
 export default app;
